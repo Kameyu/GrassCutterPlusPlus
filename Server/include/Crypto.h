04fe336815f978d4dd8db390e27520015128552a
Kameyu <1890598+Kameyu@users.noreply.github.com>
2023-01-18 04:18:38 +0100

    project structure revamp
    Look mom, I tidied up my room. Libs went static (were shared), edited file functions and added a (forced) shutdown. And more.

---
+++ Server/include/Crypto.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <cstdint>
+#include <string>
+#include "cryptopp/rsa.h"
+
+class Crypto final
+{
+private:
+	std::string DISPATCH_KEY;
+	std::string DISPATCH_SEED;
+	std::string ENCRYPT_KEY;
+	std::string ENCRYPT_SEED_BUFFER;
+	std::string SESSION_KEY;
+	uint64_t ENCRYPT_SEED = {}; // must be 8 bytes
+	CryptoPP::RSA::PrivateKey CUR_SIGNING_KEY;
+	std::map<const int, CryptoPP::RSA::PublicKey> EncryptionKeys = {};
+
+	friend class Grasscutter; // Befriend class Grasscutter
+
+public:
+	Crypto() = default;
+	~Crypto() = default;
+
+	// Explicit declaration to prevent unexpected behavior
+	Crypto(const Crypto&) = delete;
+	Crypto& operator =(Crypto const&) = delete;
+	Crypto(Crypto&&) = delete;
+	Crypto& operator=(Crypto&&) = delete;
+
+	static void _xor(char* packet, const char* key);
+	uint8_t loadKeys();
+	void createSessionKey(int length);
+
+	//Getters
+	[[nodiscard]] std::string getDispatchKey() const { return DISPATCH_KEY; }
+	[[nodiscard]] std::string getDispatchSeed() const { return DISPATCH_SEED; }
+	[[nodiscard]] std::string getEncryptKey() const { return ENCRYPT_KEY; }
+	[[nodiscard]] std::string getEncryptSeedBuffer() const { return ENCRYPT_SEED_BUFFER; }
+	[[nodiscard]] std::string getSessionKey() const { return SESSION_KEY; }
+	[[nodiscard]] uint64_t getEncryptSeed() const { return ENCRYPT_SEED; }
+
+	//Setters
+	void setDispatchKey(const std::string& buf);
+	void setDispatchSeed(const std::string& buf);
+	void setEncryptKey(const std::string& buf);
+	void setEncryptSeedBuffer(const std::string& buf);
+	void setSigningKey(const std::string& path); // Handled by CryptoPP
+	void setSessionKey(const std::string& buf);
+	void setEncryptSeed(uint64_t);
+};
---
+++ Server/include/Utils.h
@@ -0,0 +1,88 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <vector>
+
+inline const std::string RC_DIR("../../resources/");
+
+enum E_ErrorLevel
+{
+	CRITICAL,
+	IMPORTANT,
+	NORMAL
+};
+
+inline std::string getFileContent(const std::string& path)
+{
+	try
+	{
+		std::ifstream input(path, std::ios::binary);
+
+		if (!input.good())
+		{
+			throw std::runtime_error("Could not open file");
+		}
+
+		// copy all data into buffer
+		const std::vector buffer(std::istreambuf_iterator(input), {});
+		return buffer.data();
+		/* NOTE: return const buffer on purpose
+		 * cons of such an operation : if not used for a long time, buffer might be deallocated
+		 * pros : faster processing and lower memory cost. If edit needed we can still copy the buffer
+		 */
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "getFileContent FAILED: " << e.what() << std::endl;
+	}
+	return "";
+}
+
+inline void dump16(char* str, const int len)
+{
+	char* p = str;
+	for (int n = 0; n < len; ++n)
+	{
+		printf("%2.2x ", *p);
+		++p;
+	}
+
+	printf("\t");
+
+	p = str;
+	for (int n = 0; n < len; ++n)
+	{
+		printf("%c", *p ? *p : ' ');
+		++p;
+	}
+	printf("\n");
+}
+
+inline int abilityHash(const std::string& str) {
+	int v7 = 0;
+	int v8 = 0;
+	while (v8 < static_cast<int>(str.length())) {
+		v7 = str.at(v8++) + 131 * v7;
+	}
+	return v7;
+}
+
+inline void shutdown(const E_ErrorLevel err, const std::string& msg)
+{
+	std::string prefix;
+	switch (err)
+	{
+	case CRITICAL:
+		prefix = "CRITICAL";
+		break;
+	case IMPORTANT:
+		prefix = "IMPORTANT";
+		break;
+	case NORMAL:
+		prefix = "NORMAL";
+		break;
+	}
+	std::cerr << "[" << prefix << "] " << "Shutdown: " << msg << std::endl;
+	exit(err);
+}
